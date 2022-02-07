06cf86a4e2953a01243f662e2a7a6b8a65480d8b
infamousdegen <83490717+infamousdegen@users.noreply.github.com>
2022-02-07 14:20:49 +0530

    Update body.py

--- body.py
+++ body.py
@@ -13,7 +13,7 @@ from config import create_api
 api = create_api()
 
 
-def check_for_followers(api,recent_following_user1,user_1,recent_following_user2,user_2,recent_following_user3,user_3,recent_following_user4,user_4,recent_following_user5,user_5,recent_following_user6,user_6,recent_following_user7,user_7,recent_following_user8,user_8,recent_following_user9,user_9,recent_following_user10,user_10,recent_following_user11,user_11):
+def check_for_followers(api,recent_following_user1,user_1,recent_following_user2,user_2,recent_following_user3,user_3,recent_following_user4,user_4,recent_following_user5,user_5,recent_following_user6,user_6,recent_following_user7,user_7,recent_following_user8,user_8,recent_following_user9,user_9,recent_following_user10,user_10,recent_following_user11,user_11,recent_following_user12,user_12,recent_following_user13,user_13,recent_following_user14,user_14):
 
     while True:
 
@@ -207,6 +207,49 @@ def check_for_followers(api,recent_following_user1,user_1,recent_following_user2
                 time.sleep(5)
         time.sleep(180)
 
+        
+        for user in tweepy.Cursor(api.friends, screen_name=user_12).items(5):
+            time.sleep(10)
+            print("checking for new followings of "+user_12)
+            latest_following = str(user.screen_name)
+            if (latest_following not in recent_following_user11):
+                recent_following_user12.append(latest_following)
+                message=user_12+" just followed " + "https://twitter.com/"+latest_following
+                webhook = DiscordWebhook(url="https://discord.com/api/webhooks/937756263184466001/tXdqn3HAgvWOD9ewRBv3AbOXia1ORhWZ86qrXjZHvJb5QXra8WnAa4V4ol-gL1I3ihIo",content=message)
+                response = webhook.execute()
+                base_url='https://api.telegram.org/bot5230523338:AAG_rRN7sCWq29CulfA-6zfYnE2C-Ej-UDk/sendMessage?chat_id=-1001180497796&text={}'.format(message)
+                requests.get(base_url)
+                time.sleep(5)
+        time.sleep(180)
+        
+        for user in tweepy.Cursor(api.friends, screen_name=user_13).items(5):
+            time.sleep(10)
+            print("checking for new followings of "+user_13)
+            latest_following = str(user.screen_name)
+            if (latest_following not in recent_following_user11):
+                recent_following_user13.append(latest_following)
+                message=user_13+" just followed " + "https://twitter.com/"+latest_following
+                webhook = DiscordWebhook(url="https://discord.com/api/webhooks/937756263184466001/tXdqn3HAgvWOD9ewRBv3AbOXia1ORhWZ86qrXjZHvJb5QXra8WnAa4V4ol-gL1I3ihIo",content=message)
+                response = webhook.execute()
+                base_url='https://api.telegram.org/bot5230523338:AAG_rRN7sCWq29CulfA-6zfYnE2C-Ej-UDk/sendMessage?chat_id=-1001180497796&text={}'.format(message)
+                requests.get(base_url)
+                time.sleep(5)
+        time.sleep(180)
+        
+        
+        for user in tweepy.Cursor(api.friends, screen_name=user_14).items(5):
+            time.sleep(10)
+            print("checking for new followings of "+user_14)
+            latest_following = str(user.screen_name)
+            if (latest_following not in recent_following_user11):
+                recent_following_user14.append(latest_following)
+                message=user_14+" just followed " + "https://twitter.com/"+latest_following
+                webhook = DiscordWebhook(url="https://discord.com/api/webhooks/937756263184466001/tXdqn3HAgvWOD9ewRBv3AbOXia1ORhWZ86qrXjZHvJb5QXra8WnAa4V4ol-gL1I3ihIo",content=message)
+                response = webhook.execute()
+                base_url='https://api.telegram.org/bot5230523338:AAG_rRN7sCWq29CulfA-6zfYnE2C-Ej-UDk/sendMessage?chat_id=-1001180497796&text={}'.format(message)
+                requests.get(base_url)
+                time.sleep(5)
+        time.sleep(180)
 
 
 
@@ -319,6 +362,29 @@ for user in tweepy.Cursor(api.friends, screen_name=user_11).items(10):
 time.sleep(40)
 
 
+recent_following_user12=[]
+user_12="fomosaurus)"
+for user in tweepy.Cursor(api.friends, screen_name=user_12).items(10):
+    new_follower=str(user.screen_name)
+    recent_following_user11.append(new_follower)
+time.sleep(40)
+
+
+recent_following_user13=[]
+user_13="@wassiecapital"
+for user in tweepy.Cursor(api.friends, screen_name=user_13).items(10):
+    new_follower=str(user.screen_name)
+    recent_following_user11.append(new_follower)
+time.sleep(40)
+
+recent_following_user14=[]
+user_14="@Ace_da_Book"
+for user in tweepy.Cursor(api.friends, screen_name=user_14).items(10):
+    new_follower=str(user.screen_name)
+    recent_following_user11.append(new_follower)
+time.sleep(40
+
+
 
 
 time.sleep(40)
@@ -330,7 +396,7 @@ time.sleep(40)
 while True:
     print("collected initial datas")
     time.sleep(119)
-    check_for_followers(api,recent_following_user1,user_1,recent_following_user2,user_2,recent_following_user3,user_3,recent_following_user4,user_4,recent_following_user5,user_5,recent_following_user6,user_6,recent_following_user7,user_7,recent_following_user8,user_8,recent_following_user9,user_9,recent_following_user10,user_10,recent_following_user11,user_11)
+    check_for_followers(api,recent_following_user1,user_1,recent_following_user2,user_2,recent_following_user3,user_3,recent_following_user4,user_4,recent_following_user5,user_5,recent_following_user6,user_6,recent_following_user7,user_7,recent_following_user8,user_8,recent_following_user9,user_9,recent_following_user10,user_10,recent_following_user11,user_11,recent_following_user12,user_12,recent_following_user13,user_13,recent_following_user14,user14)
     time.sleep(10*5)
 print('\n\n\n                   finished following all                           ')
 
