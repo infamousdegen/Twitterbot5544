ea596ce63e233fa6eda31498a2d372b44c2d7aa6
infamousdegen <83490717+infamousdegen@users.noreply.github.com>
2022-06-01 10:23:37 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,1 +1,2 @@
-key = ['enter you twitter developer account keys here']
+key = ['R3SizhcQ1SJdpzlfQrrlCNmWL','4MLZXhCmzBMMWWVprpt1yms8rAcvgEsJBqj4oXBuAqhJp7yjbi'
+      '1531858014600593408-jqvcwzPiqpdDzamu1ip4Fw9XnJqZFG','ZiZRaAwiEBEbYBekiUznOgsTk2F9kgRPyptzD46NkhS3N']
