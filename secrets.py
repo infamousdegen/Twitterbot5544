a330aff48737bae34c618155e9ca8a043f140397
infamousdegen <83490717+infamousdegen@users.noreply.github.com>
2022-08-24 00:36:25 +0530

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,2 +1,5 @@
-key = ['R3SizhcQ1SJdpzlfQrrlCNmWL','4MLZXhCmzBMMWWVprpt1yms8rAcvgEsJBqj4oXBuAqhJp7yjbi'
-      '1531858014600593408-jqvcwzPiqpdDzamu1ip4Fw9XnJqZFG','ZiZRaAwiEBEbYBekiUznOgsTk2F9kgRPyptzD46NkhS3N']
+key = ['9orj7tded8ySzqN2tnNvn4e9L',
+      'NKWhmPzlmfyTaKZeOCxCKvK7vsQsIflMNh1Zyw5q214WLPbTxt',
+      '1487087668098117635-Xm1yslgAJINKry7D7b7PVkN7zKdLX0',
+      'm4xwPQ4AibgL5kaSeBVgatCM8OpTDODaqjXXEgNyNy3if'
+]
