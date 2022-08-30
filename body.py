df741ec9f405774a71565c8fcfe2326d883930f8
infamousdegen <83490717+infamousdegen@users.noreply.github.com>
2022-08-30 14:09:13 +0530

    Update body.py

--- body.py
+++ body.py
@@ -13,7 +13,7 @@ from config import create_api
 api = create_api()
 
 
-def check_for_followers(api,recent_following_user1,user_1,recent_following_user2,user_2,recent_following_user3,user_3,recent_following_user4,user_4,recent_following_user5,user_5,recent_following_user6,user_6,recent_following_user7,user_7,recent_following_user8,user_8,recent_following_user10,user_10,recent_following_user11,user_11,recent_following_user12,user_12,recent_following_user13,user_13,recent_following_user14,user_14):
+def check_for_followers(api,recent_following_user1,user_1,recent_following_user2,user_2,recent_following_user3,user_3,recent_following_user4,user_4,recent_following_user5,user_5,recent_following_user6,user_6,recent_following_user8,user_8,recent_following_user10,user_10,recent_following_user11,user_11,recent_following_user12,user_12,recent_following_user13,user_13,recent_following_user14,user_14):
 
     while True:
 
@@ -316,15 +316,6 @@ for user in tweepy.Cursor(api.friends, screen_name=user_6).items(10):
 
 time.sleep(40)
 
-recent_following_user7=[]
-user_7="defikhalil"
-for user in tweepy.Cursor(api.friends, screen_name=user_7).items(10):
-    new_follower=str(user.screen_name)
-    recent_following_user7.append(new_follower)
-
-
-time.sleep(40)
-
 recent_following_user8=[]
 user_8="cryptodetweiler"
 for user in tweepy.Cursor(api.friends, screen_name=user_8).items(10):
@@ -396,7 +387,7 @@ time.sleep(40)
 while True:
     print("collected initial datas")
     time.sleep(119)
-    check_for_followers(api,recent_following_user1,user_1,recent_following_user2,user_2,recent_following_user3,user_3,recent_following_user4,user_4,recent_following_user5,user_5,recent_following_user6,user_6,recent_following_user7,user_7,recent_following_user8,user_8,recent_following_user10,user_10,recent_following_user11,user_11,recent_following_user12,user_12,recent_following_user13,user_13,recent_following_user14,user_14)
+    check_for_followers(api,recent_following_user1,user_1,recent_following_user2,user_2,recent_following_user3,user_3,recent_following_user4,user_4,recent_following_user5,user_5,recent_following_user6,user_6,recent_following_user8,user_8,recent_following_user10,user_10,recent_following_user11,user_11,recent_following_user12,user_12,recent_following_user13,user_13,recent_following_user14,user_14)
     time.sleep(10*5)
 print('\n\n\n                   finished following all                           ')
 
