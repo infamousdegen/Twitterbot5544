3c09e7805dfa8e122c18ac85621de9cb0e07a570
infamousdegen <github@infamousdegen.me>
2022-12-13 01:07:59 +0530

    Delete secrets.py

--- secrets.py
+++
@@ -1,5 +0,0 @@
-key = ['9orj7tded8ySzqN2tnNvn4e9L',
-      'NKWhmPzlmfyTaKZeOCxCKvK7vsQsIflMNh1Zyw5q214WLPbTxt',
-      '1487087668098117635-Xm1yslgAJINKry7D7b7PVkN7zKdLX0',
-      'm4xwPQ4AibgL5kaSeBVgatCM8OpTDODaqjXXEgNyNy3if'
-]
